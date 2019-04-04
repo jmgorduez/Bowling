0408ea8469dedd874b2ebb8b780e6ba5c5519d67
jmgorduez <jmgorduez@gmail.com>
2019-04-04 16:59:40 -0500

    First commit

---
+++ src/test/java/ec/com/jmgorduez/Bowling/BowlingApplicationTests.java
@@ -0,0 +1,16 @@
+package ec.com.jmgorduez.Bowling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BowlingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
